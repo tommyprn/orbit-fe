3038db86bba6131af7d876e1b62c93fa2db4c32a
tommy pranoto <tommyprn8@gmail.com>
2024-07-17 12:16:57 +0700

    risk form

--- src/views/risk-control-self-assestment/risk-form.js
+++ src/views/risk-control-self-assestment/risk-form.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import secureLocalStorage from 'react-secure-storage';
 import { connect } from 'react-redux';
 import { useFormik } from 'formik';
@@ -7,7 +7,21 @@ import { createOption } from 'src/utils/use-options';
 import { getRiskColor } from 'src/utils/use-formatter';
 import { getRiskRating } from 'src/utils/use-calculate';
 import { riskFormSchema } from './RSCAFormValidation';
-import { Typography, Divider, Button } from '@mui/material';
+import { IconFileDescription, IconPencilPlus } from '@tabler/icons';
+import {
+  Table,
+  Paper,
+  Button,
+  styled,
+  Divider,
+  TableRow,
+  TableBody,
+  TableCell,
+  TableHead,
+  Typography,
+  TableContainer,
+  TablePagination,
+} from '@mui/material';
 
 // component
 import BaseInput from 'src/components/shared/input/base-input';
@@ -19,11 +33,34 @@ const RiskForm = (props) => {
   const role = JSON.parse(secureLocalStorage.getItem('selectedRoleName')).toLowerCase();
   const { dropDown, getDropdown } = props;
 
+  const StyledTableRow = styled(TableRow)(({ theme }) => ({
+    '&:nth-of-type(odd)': {
+      backgroundColor: theme.palette.action.hover,
+    },
+    // hide last border
+    '&:last-child td, &:last-child th': {
+      border: 0,
+    },
+  }));
+
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+
   useEffect(() => {
     (async () => {
       await getDropdown();
     })();
   }, []);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    const RPP = parseInt(event.target.value, 10);
+    setRowsPerPage(RPP);
+    setPage(0);
+  };
 
   const formik = useFormik({
     initialValues: {
@@ -52,6 +89,44 @@ const RiskForm = (props) => {
     },
   });
 
+  const header = [
+    'No',
+    'Id',
+    'Proses Kerja',
+    'Kategori',
+    'Penyebab',
+    'Kronologi/ Deskripsi',
+    'Akar Permasalahan',
+    'Pemilik',
+    'Signifikansi',
+    'Aksi',
+  ];
+
+  const dummy = [
+    {
+      id: 202409070001,
+      key: 'proses',
+      root: 'akarnya',
+      cause: 'Penyebab',
+      owner: 'Bambang',
+      category: 'kategori',
+      isControl: false,
+      description: 'Deskripsi masalah',
+      significance: 'Ya',
+    },
+    {
+      id: 202409070002,
+      key: 'proses',
+      root: 'akarnya',
+      owner: 'Bambang',
+      cause: 'Penyebab',
+      category: 'kategori',
+      isControl: true,
+      significance: 'Ya',
+      description: 'Deskripsi masalah',
+    },
+  ];
+
   return (
     <PageContainer title="top issue" description="top issue Page">
       <DashboardCard>
@@ -63,16 +138,16 @@ const RiskForm = (props) => {
             style={{
               gap: '16px',
               display: 'flex',
-              justifyContent: 'space-between',
               boxSizing: 'border-box',
+              justifyContent: 'space-between',
             }}
           >
             <div
               style={{
-                gap: '24px',
                 width: '100%',
                 display: 'flex',
                 flexDirection: 'column',
+                justifyContent: 'space-between',
               }}
             >
               <BaseInput
@@ -81,43 +156,6 @@ const RiskForm = (props) => {
                 value={formik.values.workProcess}
                 formik={formik}
                 placeholder="keterangan detail proses kerja"
-              />
-
-              <BaseInput
-                id="root"
-                title="Akar permasalahan"
-                value={formik.values.root}
-                formik={formik}
-                placeholder="akar permasalahan timbulnya risiko"
-              />
-
-              <BaseInput
-                id="chronology"
-                type="quill"
-                title="Kronologi/ deskripsi kejadian"
-                value={formik.values.chronology}
-                formik={formik}
-                helperText="kronologi kejadian wajib diisi"
-                placeholder="keterangan detail risiko yang teridentifikasi"
-              />
-            </div>
-
-            <div
-              style={{
-                gap: '20.5px',
-                width: '100%',
-                display: 'flex',
-                flexDirection: 'column',
-              }}
-            >
-              <BaseInput
-                id="category"
-                title="Kategori kejadian"
-                type="select"
-                value={formik.values.category}
-                formik={formik}
-                option={createOption(dropDown?.caseCategory.levelOne)}
-                placeholder="kategori kejadian risiko opr"
               />
 
               <BaseInput
@@ -131,11 +169,11 @@ const RiskForm = (props) => {
               />
 
               <BaseInput
-                id="significance"
-                title="Risiko signifikan"
-                value={formik.values.significance}
+                id="root"
+                title="Akar permasalahan"
+                value={formik.values.root}
                 formik={formik}
-                placeholder="apakah risiko ini merupakan risiko signifikan?"
+                placeholder="akar permasalahan timbulnya risiko"
               />
 
               <BaseInput
@@ -144,6 +182,45 @@ const RiskForm = (props) => {
                 value={formik.values.owner}
                 formik={formik}
                 placeholder="nama pejabat tertinggi pada unit kerja pemilik risiko"
+              />
+            </div>
+
+            <div
+              style={{
+                width: '100%',
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'space-between',
+              }}
+            >
+              <BaseInput
+                id="category"
+                title="Kategori kejadian"
+                type="select"
+                value={formik.values.category}
+                formik={formik}
+                option={createOption(dropDown?.caseCategory.levelOne)}
+                placeholder="kategori kejadian risiko opr"
+              />
+
+              <div style={{ marginBottom: '16px' }}>
+                <BaseInput
+                  id="chronology"
+                  type="quill"
+                  title="Kronologi/ deskripsi kejadian"
+                  value={formik.values.chronology}
+                  formik={formik}
+                  helperText="kronologi kejadian wajib diisi"
+                  placeholder="keterangan detail risiko yang teridentifikasi"
+                />
+              </div>
+
+              <BaseInput
+                id="significance"
+                title="Risiko signifikan"
+                value={formik.values.significance}
+                formik={formik}
+                placeholder="apakah risiko ini merupakan risiko signifikan?"
               />
             </div>
           </div>
@@ -215,7 +292,81 @@ const RiskForm = (props) => {
         </form>
       </DashboardCard>
 
-      <DashboardCard></DashboardCard>
+      <DashboardCard>
+        <Paper
+          sx={{
+            maxWidth: '100%',
+            overflow: 'hidden',
+          }}
+          elevation={0}
+          variant="outlined"
+        >
+          <TableContainer>
+            <Table size="small" aria-label="a dense table">
+              <TableHead>
+                <TableRow>
+                  {header.map((item) => (
+                    <TableCell>{item}</TableCell>
+                  ))}
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {dummy.map((row, index) => {
+                  return (
+                    <StyledTableRow
+                      key={index}
+                      sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                    >
+                      <TableCell>{index + 1}</TableCell>
+                      <TableCell>{row.id}</TableCell>
+                      <TableCell>{row.key}</TableCell>
+                      <TableCell>{row.category}</TableCell>
+                      <TableCell>{row.cause}</TableCell>
+                      <TableCell>{row.description}</TableCell>
+                      <TableCell>{row.root}</TableCell>
+                      <TableCell>{row.owner}</TableCell>
+                      <TableCell>{row.significance}</TableCell>
+                      <TableCell>
+                        <Button
+                          size="small"
+                          color={row.isControl ? 'primary' : 'warning'}
+                          variant="contained"
+                          startIcon={
+                            row.isControl ? (
+                              <IconFileDescription size="20px" />
+                            ) : (
+                              <IconPencilPlus size="20px" />
+                            )
+                          }
+                          onClick={() => {}}
+                        >
+                          {row.isControl ? 'Control' : 'Add'}
+                        </Button>
+                      </TableCell>
+                    </StyledTableRow>
+                  );
+                })}
+              </TableBody>
+            </Table>
+          </TableContainer>
+          <TablePagination
+            sx={{
+              display: 'flex',
+              width: '100%',
+              alignItems: 'center',
+              justifyContent: 'center',
+              marginTop: 1,
+            }}
+            page={page}
+            count={0}
+            component="div"
+            rowsPerPage={rowsPerPage}
+            onPageChange={handleChangePage}
+            labelRowsPerPage="Baris per halaman"
+            onRowsPerPageChange={handleChangeRowsPerPage}
+          />
+        </Paper>
+      </DashboardCard>
     </PageContainer>
   );
 };
